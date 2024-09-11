885ed582f5d6548b6a0e13bd44242faa15e264cc
dianajoseph12 <jdiana099@gmail.com>
2024-09-11 13:16:47 +0530

    comments

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#package imports
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceBgeEmbeddings
